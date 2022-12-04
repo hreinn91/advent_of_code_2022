63c2a5f84f4f7d90556171f74103c7f26d54e74d
hreinn91 <hreinnjuliusson@gmail.com>
2022-12-04 19:00:41 +0100

    Multpiple workspaces rust project (one binary crate per advent of code day solution)

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -32,14 +32,14 @@ fn get_elf_with_most_calories(file: &str) -> (usize, i32) {
         });
     println!("\ntop three: {} {} {}", top_three_calories[0],  top_three_calories[1],  top_three_calories[2]);
     let top_sum: i32 = top_three_calories.iter().sum();
-    println!("sun: {} ", top_sum);
+    println!("sum: {} ", top_sum);
     return elf_with_most_calories;
 }
 
-//Part A + B: input1.txt test1.txt
+//Part A + B: day_1/input1.txt day_1/test1.txt
 
 fn main() {
-    let elf_with_most_calories = get_elf_with_most_calories("input1.txt");
+    let elf_with_most_calories = get_elf_with_most_calories("day_1/input1.txt");
     println!(
         "Elf with most calories is elf number {} with {} calories",
         elf_with_most_calories.0, elf_with_most_calories.1
